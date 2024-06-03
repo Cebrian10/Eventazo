575aeb4bc60244e615d16d184187105d3f00582f
Ameth Cebrian <aldaircebrian@hotmail.com>
2024-06-03 17:15:18 -0500

    add frontend

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { BlogComponent } from './views/blog/blog.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: 'home', component: HomeComponent },
+    // { path: 'home', loadChildren: () => import('./views/home/home.component').then(m => m.HomeComponent) },
+    // { path: 'blog', loadChildren: () => import('./views/blog/blog.component').then(m => m.BlogComponent) },
+    // { path: 'contact', loadChildren: () => import('./views/contact/contact.component').then(m => m.ContactComponent) },    
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
