57752c60d1f6db67e641766f5b273c4df85aabf8
Ameth Cebrian <aldaircebrian@hotmail.com>
2024-06-04 18:53:03 -0500

    add views

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,14 @@
-// src/app/app-routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { NewsComponent } from './views/news/news.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { FaqComponent } from './views/faq/faq.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule) },
-    { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'news', component: NewsComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'faq', component: FaqComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
