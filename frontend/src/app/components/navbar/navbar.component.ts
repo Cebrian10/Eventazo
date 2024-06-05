59043f4ca3acca14a49baf2ace438eb5bc414587
Ameth Cebrian <aldaircebrian@hotmail.com>
2024-06-05 12:31:40 -0500

    add footer

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,95 +1,30 @@
-// import { Component, OnInit } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { Router } from '@angular/router';
-
-// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-// import { faHome, faPencilAlt, faPhone, faComments, IconDefinition, faCircleUser, 
-//          faNewspaper, faCircleQuestion ,faSpinner } from '@fortawesome/free-solid-svg-icons';
-
-//          import { MenubarModule } from 'primeng/menubar';
-
-// @Component({
-//   selector: 'app-navbar',
-//   standalone: true,
-//   imports: [CommonModule, FontAwesomeModule],
-//   templateUrl: './navbar.component.html',
-//   styleUrls: ['./navbar.component.scss']
-// })
-// export class NavbarComponent implements OnInit {
-//   userRegular: any = {};
-//   userPromoter: any = {};
-//   userAdmin: any = {};
-
-//   faHome = faHome;
-//   faNewspaper = faNewspaper;
-//   faPhone = faPhone;
-//   faCircleQuestion = faCircleQuestion;
-
-//   faPencilAlt = faPencilAlt;
-
-//   faComments = faComments;
-//   faCircleUser = faCircleUser;
-//   faSpinner = faSpinner;
-
-//   constructor(private router: Router) { }
-//   currentPage: string = 'home';
-
-//   ngOnInit() {
-//     this.userRegular = [
-//       { label: 'Inicio', command: () => this.goToPage('home'), icon: 'faHome', page: 'home' },
-//       { label: 'Noticias', command: () => this.goToPage('news'), icon: 'faNewspaper', page: 'news' },
-//       { label: 'Contacto', command: () => this.goToPage('contact'), icon: 'faPhone', page: 'contact' },      
-//       { label: 'Preguntas frecuentes', command: () => this.goToPage('faq'), icon: 'faCircleQuestion', page: 'faq' },      
-//     ];
-//   }
-
-//   goToPage(page: string) {
-//     this.currentPage = page;
-//     this.router.navigate([`/${page}`])
-//   }
-
-//   getIcon(iconName: string): IconDefinition {
-//     if (iconName === 'faHome') return this.faHome;
-//     if (iconName === 'faPencilAlt') return this.faPencilAlt;
-//     if (iconName === 'faPhone') return this.faPhone;
-//     if (iconName === 'faComments') return this.faComments;
-//     if (iconName === 'faCircleUser') return this.faCircleUser;
-//     if (iconName === 'faNewspaper') return this.faNewspaper;
-//     if (iconName === 'faCircleQuestion') return this.faCircleQuestion;
-
-//     return this.faSpinner;
-//   }
-
-//   showMenu: boolean = false;
-
-//   toggleMenu(): void {
-//     this.showMenu = !this.showMenu;
-//   }
-
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
-import { BadgeModule } from 'primeng/badge';
+
 import { AvatarModule } from 'primeng/avatar';
 import { InputTextModule } from 'primeng/inputtext';
 import { CommonModule } from '@angular/common';
 import { RippleModule } from 'primeng/ripple';
 import { Router } from '@angular/router';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faHome, faPencilAlt, faPhone, faComments, IconDefinition, faCircleUser, faNewspaper, faCircleQuestion, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHome, faPencilAlt, faPhone,
+  faComments, IconDefinition, faCircleUser,
+  faNewspaper, faCircleQuestion, faSpinner
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   standalone: true,
-  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule, FontAwesomeModule]
+  imports: [MenubarModule, AvatarModule, InputTextModule, RippleModule, CommonModule, FontAwesomeModule]
 })
+
 export class NavbarComponent implements OnInit {
   userRegular: MenuItem[] | undefined;
-  currentPage: string = 'home';
 
   faHome = faHome;
   faNewspaper = faNewspaper;
@@ -114,7 +49,6 @@ export class NavbarComponent implements OnInit {
   }
 
   goToPage(page: string) {
-    this.currentPage = page;
     this.router.navigate([`/${page}`])
   }
 
@@ -130,9 +64,4 @@ export class NavbarComponent implements OnInit {
     return this.faSpinner;
   }
 
-  showMenu: boolean = false;
-
-  toggleMenu(): void {
-    this.showMenu = !this.showMenu;
-  }
 }
